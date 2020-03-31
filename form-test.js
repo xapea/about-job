917a0d287c483b03582c9c0e9f0d5f9f9d9db4ba
xapea <30462431+xapea@users.noreply.github.com>
2020-04-01 07:22:40 +0800

    updated

--- form-test.js
+++ form-test.js
@@ -1,21 +1,15 @@
 let sendButton = document.querySelector('button');
 
 function send() {
-  let name = document.querySelector('#form_field_1Value').value;
-  let phone = document.querySelector('#form_field_2Value').value;
-  let demand = document.querySelector('#form_field_3Value').value;
-  let demand = document.querySelector('#form_field_4Value').value;
-  let demand = document.querySelector('#form_field_5Value').value;
-  let demand = document.querySelector('#form_field_6Value').value;
+  let name = document.querySelector('#nameValue').value;
+  let phone = document.querySelector('#phoneValue').value;
+  let demand = document.querySelector('#demandValue').value;
   $.ajax({
     url: "https://script.google.com/macros/s/AKfycby19TAEMJSkwFIb6Ka-O5gYo-en446w1BL5N9oJo59ON0-oemU/exec",
     data: {
-        "form_field_1": form_field_1,
-        "form_field_2": form_field_2,
-        "form_field_3": form_field_3,
-        "form_field_4": form_field_4,
-        "form_field_5": form_field_5,
-        "form_field_6": form_field_6
+        "name": name,
+        "phone": phone,
+        "demand": demand
     },
     success: function(response) {
       if(response == "成功"){
